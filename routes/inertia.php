a0260838b2154efe388670024e2db2dd6ec65251
seiyu-nico <yuseiishii141@gmail.com>
2024-07-24 21:00:18 +0900

    inertiaと関係パッケージ追加

---
+++ routes/inertia.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Middleware\HandleInertiaRequests;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::middleware([HandleInertiaRequests::class])->group(function () {
+});
